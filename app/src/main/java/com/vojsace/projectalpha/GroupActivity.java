a5ce1102612ea4dd7aa6fa6b9fa1013d6286d14f
vojsace <vojsace@github.com>
2020-05-07 19:46:23 +0200

    Home screen added with username get improved

--- app/src/main/java/com/vojsace/projectalpha/GroupActivity.java
+++ app/src/main/java/com/vojsace/projectalpha/GroupActivity.java
@@ -2,22 +2,34 @@ package com.vojsace.projectalpha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class GroupActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView room_name;
+    private String name;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
 
-        textView = (TextView) findViewById(R.id.group_text);
+        room_name = (TextView) findViewById(R.id.group_text);
 
+        request_user_name();
+        room_name.setText(name);
 
-        String name = getIntent().getExtras().getString("user_name");
-        textView.setText(name);
     }
+
+    private void request_user_name(){
+        name = getIntent().getExtras().getString("user_name");
+
+    }
+
 }
