67d3713685f6e7914d682712b768276b216a85c2
vojsace <vojsace@github.com>
2020-05-12 01:42:24 +0200

    Added RecyclerView

--- app/src/main/java/com/vojsace/projectalpha/ChatActivity.java
+++ app/src/main/java/com/vojsace/projectalpha/ChatActivity.java
@@ -3,15 +3,21 @@ package com.vojsace.projectalpha;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,31 +30,34 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
+    private Button btnSave;
+    private EditText msg_input;
+    private RecyclerView recyclerView;
 
-    private Button btn_send_msg;
-    private EditText input_msg;
-    private TextView chat_conversation;
-
-    private String user_name, room_name;
+    DatabaseReference ref;
+    private String room_name, current_user;
     private String temp_key;
-    private DatabaseReference ref;
+
+    private FirebaseRecyclerOptions<model> options;
+    private FirebaseRecyclerAdapter<model, myViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        btn_send_msg = (Button) findViewById(R.id.chat_btn);
-        input_msg = (EditText)findViewById(R.id.chat_editText);
-        chat_conversation = (TextView) findViewById(R.id.textMsgView);
-
-        user_name = getIntent().getExtras().get("user_name").toString();
+        current_user = getIntent().getExtras().get("user_name").toString();
         room_name = getIntent().getExtras().get("room_name").toString();
         setTitle("Room - " + room_name);
 
+        btnSave = (Button)findViewById(R.id.chat_btn);
+        msg_input = (EditText)findViewById(R.id.chat_editText);
         ref = FirebaseDatabase.getInstance().getReference("rooms/" + room_name);
+        recyclerView = findViewById(R.id.recyclerViewID);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        btn_send_msg.setOnClickListener(new View.OnClickListener() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Map<String, Object> map = new HashMap<String, Object>();
@@ -57,50 +66,32 @@ public class ChatActivity extends AppCompatActivity {
 
                 DatabaseReference message_root = ref.child(temp_key);
                 Map<String, Object> map2 = new HashMap<String, Object>();
-                map2.put("name", user_name);
-                map2.put("msg", input_msg.getText().toString());
+                map2.put("name", current_user);
+                map2.put("msg", msg_input.getText().toString());
 
                 message_root.updateChildren(map2);
-                input_msg.setText("");
-            }
-        });
-        ref.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                append_chat_conversation(dataSnapshot);
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                append_chat_conversation(dataSnapshot);
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                msg_input.setText("");
             }
         });
 
-    }
-    private String chat_msg, chat_user_name;
-    private void append_chat_conversation(DataSnapshot dataSnapshot){
-        Iterator i = dataSnapshot.getChildren().iterator();
+        options = new FirebaseRecyclerOptions.Builder<model>().setQuery(ref, model.class).build();
+        adapter = new FirebaseRecyclerAdapter<model, myViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull myViewHolder myViewHolder, int i, @NonNull model model) {
+                myViewHolder.textViewUser.setText(model.getName());
+                myViewHolder.textViewMsg.setText(model.getMsg());
+            }
 
-        while(i.hasNext()){
-            chat_msg = (String) (((DataSnapshot)i.next()).getValue());
-            chat_user_name = (String) (((DataSnapshot)i.next()).getValue());
+            @NonNull
+            @Override
+            public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+              View v =  LayoutInflater.from(parent.getContext()).inflate(R.layout.single_message_layout, parent, false);
 
-            chat_conversation.append(chat_user_name + " : " + chat_msg + " \n");
-        }
+                return new myViewHolder(v);
+            }
+        };
+
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/vojsace/projectalpha/model.java
@@ -0,0 +1,30 @@
+package com.vojsace.projectalpha;
+
+public class model {
+    private String msg;
+    private String name;
+
+    public model() {
+    }
+
+    public model(String msg, String name) {
+        this.msg = msg;
+        this.name = name;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
