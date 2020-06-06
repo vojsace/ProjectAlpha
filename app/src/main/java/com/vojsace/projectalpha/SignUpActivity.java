b86b033ed6316a1255c8129fcc30fbce981a2e86
vojsace <vojsace@gmail.com>
2020-06-06 19:02:01 +0200

    unique username

--- app/src/main/java/com/vojsace/projectalpha/SignUpActivity.java
+++ app/src/main/java/com/vojsace/projectalpha/SignUpActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,8 +19,12 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Random;
 
@@ -29,6 +34,7 @@ private Button signUpBtn;
 private FirebaseAuth auth;
 
 private DatabaseReference reference;
+    private DatabaseReference mDatabaseUsername;
 
 private String username, email, pass;
 
@@ -41,8 +47,10 @@ private String username, email, pass;
         signUpMail = (TextInputLayout) findViewById(R.id.email_registerID);
         signUpPass = (TextInputLayout) findViewById(R.id.pass_registerID);
         reference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseUsername = FirebaseDatabase.getInstance().getReference().child("Users");
         auth = FirebaseAuth.getInstance();
         signUpBtn = (Button) findViewById(R.id.signUp_btn);
+
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,41 +58,60 @@ private String username, email, pass;
                 username = signUpUsername.getEditText().getText().toString();
                 email = signUpMail.getEditText().getText().toString();
                 pass = signUpPass.getEditText().getText().toString();
+                final Query usernameQuery = mDatabaseUsername.orderByChild("username").equalTo(username);
 
-                if (username.isEmpty()){
-                    signUpUsername.setError("Please enter your Username");
-                }else
-                    signUpUsername.setError(null);
-                if (email.isEmpty()){
-                    signUpMail.setError("Please enter your E-mail");
-                }else
-                    signUpMail.setError(null);
-                if (pass.isEmpty()){
-                    signUpPass.setError("Please enter a Password");
-                }else if (pass.length() < 8){
-                    signUpPass.setError("Password too weak");
-                }else
-                    signUpPass.setError(null);
+                usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.getChildrenCount() > 0){
+                            Log.d("usernameExists", "already taken");
+                            signUpUsername.setError("Username already taken");
+                        }else{
+                            Log.d("usernameExists", "NOT taken");
+                            signUpUsername.setError(null);
 
-                    if (!username.isEmpty() && !email.isEmpty() && !pass.isEmpty()) {
-                        auth.createUserWithEmailAndPassword(email, pass)
-                                .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if (!task.isSuccessful()) {
-                                            Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            String uId = auth.getUid();
-                                            writeNewUser(uId, username);
-                                            startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
-                                            finish();
-                                        }
+                            if (username.isEmpty()){
+                                signUpUsername.setError("Please enter your Username");
+                            }else
+                                signUpUsername.setError(null);
+                            if (email.isEmpty()){
+                                signUpMail.setError("Please enter your E-mail");
+                            }else
+                                signUpMail.setError(null);
+                            if (pass.isEmpty()){
+                                signUpPass.setError("Please enter a Password");
+                            }else if (pass.length() < 8){
+                                signUpPass.setError("Password too weak");
+                            }else
+                                signUpPass.setError(null);
 
-                                    }
-                                });
-                    }else {
-                        Toast.makeText(SignUpActivity.this, "Error Occupied", Toast.LENGTH_SHORT).show();
+                            if (!username.isEmpty() && !email.isEmpty() && !pass.isEmpty()) {
+                                auth.createUserWithEmailAndPassword(email, pass)
+                                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (!task.isSuccessful()) {
+                                                    Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    String uId = auth.getUid();
+                                                    writeNewUser(uId, username);
+                                                    startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                                                    finish();
+                                                }
+
+                                            }
+                                        });
+                            }else {
+                                Toast.makeText(SignUpActivity.this, "Error Occupied", Toast.LENGTH_SHORT).show();
+                            }
+                        }
                     }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
@@ -100,7 +127,7 @@ private String username, email, pass;
         UserInfo userInfo = new UserInfo();
         userInfo.setUsername(username);
         userInfo.setUser_color(Color.rgb(r, g, b));
+            reference.child("Users").child(userId).setValue(userInfo);
 
-        reference.child("Users").child(userId).setValue(userInfo);
     }
 }
